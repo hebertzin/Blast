60aeee467d3ef7a1f23ad7bc42f8e6db507ae659
hebertsanto <hebertsantos0704@gmail.com>
2024-04-26 12:16:19 -0300

    add importants packages to the project including security and much more

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import express from 'express'
+import helmet from 'helmet'
+import morgan from 'morgan'
+import compression from 'compression'
 import { env } from './config/env'
 import { upload } from './routes/upload'
 import { logger } from './config/logger/winston'
 
 const app = express()
 
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(compression())
+app.use(helmet())
+
+app.use(
+  morgan(function (tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, 'content-length'),
+      '-',
+      tokens['response-time'](req, res),
+      'ms',
+    ].join(' ')
+  }),
+)
 
 app.use('/api/v1', upload)
 
