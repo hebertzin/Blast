608a14f511c0a13615ebd97f625a448bb8f9cd8b
hebertsanto <hebertsantos0704@gmail.com>
2024-06-22 00:44:32 -0300

    chore: config env variables

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,17 +1,23 @@
 import 'dotenv/config'
-
 import { z } from 'zod'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3000),
+  PORT: z.coerce.number(),
   ACCESS_KEY: z.string(),
   SECRET_KEY: z.string(),
   REGION: z.string(),
 })
 
-const _env = envSchema.safeParse(process.env)
+let _env
+const isTestEnvironment = process.env.NODE_ENV === 'test'
 
-if (_env.success == false) {
+if (isTestEnvironment) {
+  _env = envSchema.partial().safeParse(process.env)
+} else {
+  _env = envSchema.strict().safeParse(process.env)
+}
+
+if (!_env.success) {
   throw new Error('Invalid environment variables')
 }
 
