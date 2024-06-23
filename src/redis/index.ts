4260a76323e46093935f1e5f288d51f6727ce3ae
hebertsanto <hebertsantos0704@gmail.com>
2024-06-23 17:32:45 -0300

    feat : setup redis

---
+++ src/redis/index.ts
@@ -0,0 +1,8 @@
+import { Redis } from 'ioredis'
+import { env } from '../config/env'
+
+export const redis = new Redis({
+  password: env.REDIS_PASSWORD,
+  host: env.REDIS_HOST,
+  port: env.PORT,
+})
